d7944130c3195b2c051fcc044c5cf77a5aebcc32
danielkenji01 <daniel_kenji_1@hotmail.com>
2021-05-21 11:35:55 -0300

    aplicação da regra de negócio do terceiro caso de teste

--- src/main/java/com/fatec/Biblioteca.java
+++ src/main/java/com/fatec/Biblioteca.java
@@ -1,5 +1,7 @@
 package com.fatec;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,14 +10,33 @@ public class Biblioteca {
     private List<Livro> livros = new ArrayList<>();
 
     public boolean adicionar(Livro livro) {
-        if(livros.contains(livro)){
+
+        if (!validarCampos(livro)) {
             return false;
         }
+
+        if (livros.contains(livro)){
+            return false;
+        }
+
         return livros.add(livro);
     }
 
     public int size() {
         return livros.size();
+    }
+
+    private boolean validarCampos(Livro livro) {
+
+        if (livro == null) {
+            return false;
+        }
+
+        if (StringUtils.isBlank(livro.getIsbn())) {
+            return false;
+        }
+
+        return true;
     }
 
 }
