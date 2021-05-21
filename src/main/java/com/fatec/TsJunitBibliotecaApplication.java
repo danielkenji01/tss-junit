9bfe876ae5bf96f5ed63d676356d99dadacb9a3c
danielkenji01 <daniel_kenji_1@hotmail.com>
2021-05-21 10:40:12 -0300

    Initial commit

---
+++ src/main/java/com/fatec/TsJunitBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.fatec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsJunitBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsJunitBibliotecaApplication.class, args);
+	}
+
+}
